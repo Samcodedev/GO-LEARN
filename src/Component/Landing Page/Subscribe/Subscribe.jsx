b89f1ef6a4f942ac6af6d09cba2f7349718e7e5a
Simlex <gettosimlex@gmail.com>
2022-11-01 14:41:34 +0100

    Updated details component ui

--- src/Component/Landing Page/Subscribe/Subscribe.jsx
+++ src/Component/Landing Page/Subscribe/Subscribe.jsx
@@ -10,7 +10,7 @@ const Subscribe = () => {
                     <h2>Subscribe to Our Newsletter</h2>
                     <p>Interested in getting up-to-date information about our offers, promotions or perks?</p>
                     <form action="/construction">
-                        <input type="email" required={true}/><button>Subscribe</button>
+                        <input type="email" required={true} placeholder='Enter your email address'/><button>Subscribe Now</button>
                     </form>
                 </div>
             </div>
