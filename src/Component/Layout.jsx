2820e81d2916681c154c58977510f169d0a10752
Simlex <gettosimlex@gmail.com>
2022-11-19 18:05:38 +0100

    Added props for login status

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import NavBar from './NavBar/NavBar';
-import { Outlet } from "react-router-dom"
+import React from "react";
+import NavBar from "./NavBar/NavBar";
+import { Outlet } from "react-router-dom";
 
-const Layout = () => {
-    return(
-        <>
-            <NavBar />
-            <Outlet />
-        </>
-    )
-}
+const Layout = ({ isLoggedIn }) => {
+  return (
+    <>
+      <NavBar isLoggedIn={isLoggedIn} />
+      <Outlet />
+    </>
+  );
+};
 
 export default Layout;
