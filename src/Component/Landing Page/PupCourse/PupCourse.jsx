99e5d1a936a3c437cfc2688ce1c58cfe17a9aa62
Simlex <gettosimlex@gmail.com>
2023-01-11 13:19:33 +0100

    Updated homepage UI

--- src/Component/Landing Page/PupCourse/PupCourse.jsx
+++ src/Component/Landing Page/PupCourse/PupCourse.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 
 const PupCourse = ({landingCourses}) => {
 
+  console.log('landingCourses: ', landingCourses);
+
   return (
     <div className="pupcourse">
       <div className="sub-pupcourse">
