01a006fe44e9bad956f3d1c12ab8f737e20bfac8
Simlex <gettosimlex@gmail.com>
2023-01-16 14:18:45 +0100

    Added card conditional rendering on homepage

--- src/Component/Landing Page/Head/Head.jsx
+++ src/Component/Landing Page/Head/Head.jsx
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 import Course from "../.././Courses";
 import { BsPerson } from "react-icons/bs";
 import Card from "./Card";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Head = ({ landingCourses }) => {
+  const [tokenAvailability, setTokenAvailability] = useState(false);
+
   const data = [
     {
       title: "Cryptocurrency Trading Course",
@@ -17,7 +21,7 @@ const Head = ({ landingCourses }) => {
     return <Course title={item.title} star={item.star} />;
   });
 
-  console.log("landingCourses: ", landingCourses);
+  // console.log("landingCourses: ", landingCourses);
 
   const course = JSON.parse(localStorage.getItem("courses"));
 
@@ -28,8 +32,14 @@ const Head = ({ landingCourses }) => {
   let data1 = fashionCourses[0];
   let data2 = fashionCourses[1];
 
-  console.log("Data 1: ", data1);
-  console.log("Data 2: ", data2);
+  // console.log("Data 1: ", data1);
+  // console.log("Data 2: ", data2);
+
+  // useeffect hook to get token from localStorage and set token availability 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    token && setTokenAvailability(true);
+  }, []);
 
   return (
     <div className="head">
@@ -41,16 +51,23 @@ const Head = ({ landingCourses }) => {
           opportunities can bring a significant change in how individuals prefer
           to learn. Therefore, we offer you the beauty of eLearning.
         </p>
-        <Link to="/register">
-          <button>
-            {" "}
-            <BsPerson fontSize="20px" /> Join For Free
-          </button>
-        </Link>
+        {tokenAvailability ? (
+          <Link to="/courses">
+            <button>
+              <BsPerson fontSize="20px" /> View Courses
+            </button>
+          </Link>
+        ) : (
+          <Link to="/register">
+            <button>
+              <BsPerson fontSize="20px" /> Join For Free
+            </button>
+          </Link>
+        )}
       </div>
       <div className="head-card">
-        <div className="card-wrapper">{data1 && <Card data={data1} />}</div>
-        <div className="card-wrapper">{data2 && <Card data={data2} />}</div>
+        <div className="card-wrapper">{data1 && <Card data={data1} hideBottomVisibility={true} />}</div>
+        <div className="card-wrapper">{data2 && <Card data={data2} hideBottomVisibility={true} />}</div>
       </div>
       {/* </div> */}
     </div>
